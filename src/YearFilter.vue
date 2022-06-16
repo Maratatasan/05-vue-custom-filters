8d8b8c2f20a0415e2290b3c76c76bc33fd5ae984
Maratatasan <marats.stelihs@gmail.com>
2022-06-16 09:48:40 +0100

    filter component hello world

--- src/YearFilter.vue
+++ src/YearFilter.vue
@@ -1,60 +1,9 @@
 <template>
-  <div>
-  <div class="filter-title">My Year Filter</div>
-    <label>
-      <input
-        type="radio"
-        name="my-year-filter"
-        @change="updateFilter('All')"
-      />
-      All
-    </label>
-    <label>
-      <input
-        type="radio"
-        name="my-year-filter"
-        @change="updateFilter('2010')"
-      />
-      Since 2010
-    </label>
-  </div>
+  <div>Hello World</div>
 </template>
 
 <script>
-import { reactive, ref } from "@vue/reactivity";
-
-export default {
-  setup(props) {
-    const { params } = props;
-    let year = ref( "All" );
-
-    function updateFilter(newValue) {
-      year.value = newValue
-      params.filterChangedCallback();
-    }
-
-    function doesFilterPass({ data }) {
-      return data.year >= 2010;
-    }
-
-    function isFilterActive() {
-      return year.value === "2010";
-    }
-
-    function getModel() {}
-
-    function setModel() {}
-
-    return {
-      updateFilter,
-      doesFilterPass,
-      isFilterActive,
-      getModel,
-      setModel,
-      year,
-    };
-  },
-};
+export default {};
 </script>
 
 <style lang="scss" scoped></style>
