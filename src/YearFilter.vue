ff078075cdb95f4b26b170535c95de917747023d
Maratatasan <marats.stelihs@gmail.com>
2022-06-20 16:19:48 +0100

    radio-btn-ready

--- src/YearFilter.vue
+++ src/YearFilter.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>Hello World</div>
+  <div>
+    <div class="filter-title">Year Filter</div>
+    <label
+      v-for="(value, index) in ['Filter off', 'Filter on']"
+      :key="index"
+    >
+      <input
+        type="radio"
+        :value="value"
+        v-model="filterState"
+        @change="updateFilter()"
+      />
+      {{ value }}
+    </label>
+  </div>
 </template>
 
 <script setup>
+import { ref } from "@vue/reactivity";
+
 const { params } = defineProps();
+
+const filterState = ref("Filter off");
 
 function updateFilter() {
   params.filterChangedCallback();
@@ -13,7 +31,7 @@ function isFilterActive() {
   return false;
 }
 
-function doesFilterPass() {
+function doesFilterPass(params) {
   return false;
 }
 
